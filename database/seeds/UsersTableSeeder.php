dce7956806873dab3e15431b1b76f425034329ab
Ricardo Silitonga <ricardosilitonga81@gmail.com>
2022-11-14 23:29:29 +0700

    Create Post & User Seeder

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // reset the users table
+        DB::statement('SET FOREIGN_KEY_CHECKS=0');
+        DB::table('users')->truncate();
+
+        // generate 3 users/authors
+        DB::table('users')->insert(
+            [
+                'name' => 'JaneDoe',
+                'email' => 'janedoe@gmail.com',
+                'password' => bcrypt('secret')
+            ],
+            [
+                'name' => 'JohnDoe',
+                'email' => 'johndoe@gmail.com',
+                'password' => bcrypt('secret')
+            ],
+            [
+                'name' => 'Ricardo',
+                'email' => 'ricardo@gmail.com',
+                'password' => bcrypt('secret')
+            ]
+        );
+    }
+}
