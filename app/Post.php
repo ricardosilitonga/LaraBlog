b130d6a8c6adcc1cd09a10acc11ab5d1feed2b19
Ricardo Silitonga <ricardosilitonga81@gmail.com>
2022-11-14 23:30:33 +0700

    Ide Helper models

--- app/Post.php
+++ app/Post.php
@@ -4,6 +4,29 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Post
+ *
+ * @mixin \Eloquent
+ * @property int $id
+ * @property int $author_id
+ * @property string $title
+ * @property string $slug
+ * @property string $excerpt
+ * @property string $body
+ * @property string $image
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
+ * @method static \Illuminate\Database\Query\Builder|\App\Post whereAuthorId($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Post whereBody($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Post whereCreatedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Post whereExcerpt($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Post whereId($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Post whereImage($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Post whereSlug($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Post whereTitle($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Post whereUpdatedAt($value)
+ */
 class Post extends Model
 {
     //
