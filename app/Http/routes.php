691b5c1b30a7091009d72c1f51a3a9e2a364354f
Ricardo Silitonga <ricardosilitonga81@gmail.com>
2022-11-14 18:50:23 +0700

    Instal template frontend Blog

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,15 @@
 |
 */
 
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('blog.index');
+});
+
+Route::get('/blog/show', function () {
+    return view('blog.show');
 });
