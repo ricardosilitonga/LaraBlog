e89af51b6b15cb46b995843e338627086b67ccf2
Ricardo Silitonga <ricardosilitonga81@gmail.com>
2022-11-15 14:42:22 +0700

    New ide helper generate to fix Fluent Class

---
+++ _ide_helper_models.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * A helper file for your Eloquent Models
+ * Copy the phpDocs from this file to the correct Model,
+ * And remove them from this file, to prevent double declarations.
+ *
+ * @author Barry vd. Heuvel <barryvdh@gmail.com>
+ */
+
+
+namespace App{
+/**
+ * App\Post
+ *
+ * @mixin \Eloquent
+ * @property int $id
+ * @property int $author_id
+ * @property string $title
+ * @property string $slug
+ * @property string $excerpt
+ * @property string $body
+ * @property string $image
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
+ * @method static \Illuminate\Database\Query\Builder|\App\Post whereAuthorId($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Post whereBody($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Post whereCreatedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Post whereExcerpt($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Post whereId($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Post whereImage($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Post whereSlug($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Post whereTitle($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Post whereUpdatedAt($value)
+ * @property-read mixed $image_url
+ */
+	class Post extends \Eloquent {}
+}
+
+namespace App{
+/**
+ * App\User
+ *
+ * @mixin \Eloquent
+ * @property int $id
+ * @property string $name
+ * @property string $email
+ * @property string $password
+ * @property string $remember_token
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
+ * @method static \Illuminate\Database\Query\Builder|\App\User whereCreatedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\User whereEmail($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\User whereId($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\User whereName($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\User wherePassword($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\User whereRememberToken($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\User whereUpdatedAt($value)
+ */
+	class User extends \Eloquent {}
+}
+
