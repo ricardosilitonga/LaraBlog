061dbe0ab3ef9f59281201993ab66847c16a2d93
Ricardo Silitonga <ricardosilitonga81@gmail.com>
2022-11-16 21:41:06 +0700

    Update Post seeder

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use Faker\Factory;
+use Carbon\Carbon;
 
 class PostsTableSeeder extends Seeder
 {
@@ -17,12 +18,15 @@ class PostsTableSeeder extends Seeder
 
         $posts = [];
         $faker = Factory::create();
+        $date = Carbon::create(2022, 11, 12, 9);
 
         // generate 10 dummy posts
-        for ($i=1; $i<10; $i++) {
+        for ($i=1; $i<=10; $i++) {
 
             $image = 'Post_Image_' . rand(1, 5) . '.jpg';
-            $date = date('Y-m-d H:i:s', strtotime("2022-11-14 22:59:00 + {$i} days"));
+//            $date = date('Y-m-d H:i:s', strtotime("2022-11-14 22:59:00 + {$i} days"));
+            $date = $date->addDays(1);
+            $publishedDate = clone($date);
 
             $posts[] = [
                 'author_id' => rand(1, 3),
@@ -31,8 +35,9 @@ class PostsTableSeeder extends Seeder
                 'excerpt'  => $faker->text(rand(250,300)),
                 'body'  => $faker->paragraphs(rand(10,15), true),
                 'image' => rand(0, 1) == 1 ? $image : NULL,
-                'created_at'    => $date,
-                'updated_at'    => $date,
+                'created_at'    => clone $date,
+                'updated_at'    => clone $date,
+                'published_at'  => $i < 5 ? $publishedDate : (rand(0,1) == 0 ? NULL : $publishedDate->addDays(4))
             ];
         }
 
