5b1e1cfa7397abddce763e765556d14680e0f649
Ricardo Silitonga <ricardosilitonga81@gmail.com>
2022-12-11 10:29:39 +0700

    Add Base Backend Controller

--- app/Http/Controllers/Backend/BackendController.php
+++ app/Http/Controllers/Backend/BackendController.php
@@ -9,5 +9,13 @@ use App\Http\Controllers\Controller;
 
 class BackendController extends Controller
 {
-    //
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
 }
